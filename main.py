1ec09d365d1c802fb78a5cac334acd4a623ab74f
Paolo Izzo <pizzo@subseacraft.com>
2026-08-20 14:38:21 +0100

    rearranged pins

--- main.py
+++ main.py
@@ -16,20 +16,20 @@ from control_button import ControlButton, ControlAction, LEDMode, PatternMap
 UPDATE_INTERVAL = 5
 
 # Display
-P_DISP_DIO = 2
-P_DISP_CLK = 3
+P_DISP_DIO = 18
+P_DISP_CLK = 19
 # Neopixel
-P_NP = 15  # Data pin
+P_NP = 22  # Data pin
 NP_STRIP_LEN = 8  # Number of leds per strip
 NP_STRIP_NUM = 4  # Number of strips
 # Control pins
-P_CONTROLS = [10, 11, 12, 13]
+P_CONTROLS = [6, 7, 8, 9]
 # Encoder pins
-P_ROTARY_CLK = 16
-P_ROTARY_DT = 17
-P_ROTARY_SW = 18
+P_ROTARY_CLK = 14
+P_ROTARY_DT = 15
+P_ROTARY_SW = 13
 # Extra button pins
-P_MENU_BUTTONS = [19, 20]
+P_MENU_BUTTONS = [10, 11]
 
 MIDI_MAP = MidiMap(
     channel=0, snap_cc=24, preset_cc=20, preset_up_val=1, preset_down_val=2
@@ -37,10 +37,11 @@ MIDI_MAP = MidiMap(
 
 CONTROLS_MAP = [
     [ControlAction.SNAP_1_2, ControlAction.NONE, LEDMode.SNAP],
-    [ControlAction.SNAP_3_4, ControlAction.NONE, LEDMode.SNAP],
+    [ControlAction.SNAP_3_4, ControlAction.PRESET_DOWN, LEDMode.SNAP],
     [ControlAction.SNAP_5_6, ControlAction.PRESET_UP, LEDMode.SNAP],
-    [ControlAction.SNAP_7_8, ControlAction.PRESET_DOWN, LEDMode.SNAP],
+    [ControlAction.SNAP_7_8, ControlAction.NONE, LEDMode.SNAP],
 ]
+
 
 SNAP_PATTERN_MAP = PatternMap(
     Pulse(color1=(0, 80, 0), color2=(0, 60, 20), period_ms=5000),
